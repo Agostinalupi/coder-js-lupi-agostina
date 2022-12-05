25e4d8a7460817cb3dc340fb248a52ec206c95b9
Agostina Lupi <agostinalupi@gmail.com>
2022-12-05 11:34:46 -0300

    primera preentrega js

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,18 @@
+
+
+let usuario = prompt("Ingrese su nombre de usuario");
+console.log("Nombre de usuario " + usuario);
+
+let password = parseInt(prompt("Ingrese su contraseña"));
+console.log("contraseña:  " + password);
+
+if ((usuario == "agoslupi") && (password == "0000")) {
+    alert("Bienvenido agoslupi");
+}
+else {
+    alert("Usuario no autorizado");
+}
+
 function calcularDescuento (precio) {
  let totalDescuento = precio * 0.9
  alert("el precio total con el descuento del 10% es $"+totalDescuento);
